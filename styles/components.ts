6d82512335ca89a434ea2d415b327ee75f40971b
Deepak Suthar <codewithdeepak@yahoo.com>
2022-04-14 12:01:40 +0530

    added theme

---
+++ styles/components.ts
@@ -0,0 +1,29 @@
+export const components = {
+  Button: {
+    baseStyle: {
+      rounded: "sm",
+    },
+  },
+  Text: {
+    baseStyle: {
+      color: "dark.50",
+    },
+    defaultProps: {
+      size: "sm",
+    },
+    sizes: {
+      xl: {
+        fontSize: "64px",
+      },
+      lg: {
+        fontSize: "32px",
+      },
+      md: {
+        fontSize: "16px",
+      },
+      sm: {
+        fontSize: "12px",
+      },
+    },
+  },
+};
--- styles/palettes.ts
+++
@@ -1,7 +0,0 @@
-export const palettes = {
-  brand: {
-    900: "#8287af",
-    800: "#7c83db",
-    700: "#b3bef6",
-  },
-};
